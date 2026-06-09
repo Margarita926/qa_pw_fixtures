dc2104134c3bc6703a57735447c40c254ec54367
Margaret <margarita@medentee.com>
2026-06-09 10:36:28 +0300

    < fixture added to the test createArticleWithRequiredFields>

--- tests/createArticle/createArticleWithRequiredFields.spec.js
+++ tests/createArticle/createArticleWithRequiredFields.spec.js
@@ -1,12 +1,9 @@
 import { test } from '../_fixtures/fixtures';
-import { generateNewArticleData } from '../../src/common/testData/generateNewArticleData';
 import { signUpUser } from '../../src/ui/actions/auth/signUpUser';
 
-let article;
 
 test.beforeEach(async ({ homePage, newUserData }) => {
   await signUpUser(homePage.page, newUserData);
-  article = generateNewArticleData();
 });
 
 
@@ -14,14 +11,15 @@ test('Creat an article with required fields', async ({
   homePage,
   createArticlePage,
   viewArticlePage,
+  articleWithoutTags
 }) => {
   await homePage.clickNewArticleLink();
 
-  await createArticlePage.fillTitleField(article.title);
-  await createArticlePage.fillDescriptionField(article.description);
-  await createArticlePage.fillTextField(article.text);
+  await createArticlePage.fillTitleField(articleWithoutTags.title);
+  await createArticlePage.fillDescriptionField(articleWithoutTags.description);
+  await createArticlePage.fillTextField(articleWithoutTags.text);
   await createArticlePage.clickPublishArticleButton();
 
-  await viewArticlePage.assertArticleTitleIsVisible(article.title);
-  await viewArticlePage.assertArticleTextIsVisible(article.text);
+  await viewArticlePage.assertArticleTitleIsVisible(articleWithoutTags.title);
+  await viewArticlePage.assertArticleTextIsVisible(articleWithoutTags.text);
 });
